0b42028f85d11623dbd1c51a135e114388e128c9
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2021-12-14 18:03:29 +0100

    updated proj

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import "./App.css";
+import logo from "../src/img/HN-logo.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
+//import Card from "./components/Card";
+import BasicCard from "./components/CardMaterial";
 
-function App() {
+const App = () => {
+  const [news, setNews] = useState([]); // Set up "useState"
+  const URL = `http://hn.algolia.com/api/v1/search?tags=front_page`; // API URL
+
+  const fetchData = () => {
+    axios
+      .get(URL)
+      .then((res) => {
+        console.log(res.data.hits);
+        setNews(res.data.hits);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // console.log(news);
+
   return (
-    <div className="App">
-      <div className="logo">
-        <img src="../src/img/HN-logo.png" alt="logo" />
-      </div>
+    <div>
+      {news.map((item) => (
+        // <Card {...item} key={item.objectID} />
+        <BasicCard {...item} key={item.objectID} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
