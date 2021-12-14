96900ab82c59561a98a533489d69e1555bc0d74f
Coralee <45268389+Coralee-Long@users.noreply.github.com>
2021-12-14 23:26:07 +0100

    styles add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
-import logo from "../src/img/HN-logo.png";
 import axios from "axios";
 import { useState, useEffect } from "react";
-//import Card from "./components/Card";
 import BasicCard from "./components/CardMaterial";
+import logo from "./img/HN-logo.png";
+import { Toolbar, AppBar, Typography, Container } from "@material-ui/core";
+import useStyles from "./components/Styles";
+import Button from "@mui/material/Button";
+import { borderColor } from "@mui/system";
+import SearchIcon from "@mui/icons-material/Search";
 
 const App = () => {
+  const classes = useStyles();
   const [news, setNews] = useState([]); // Set up "useState"
   const URL = `http://hn.algolia.com/api/v1/search?tags=front_page`; // API URL
 
-  const fetchData = () => {
-    axios
+  const fetchData = async () => {
+    await axios
       .get(URL)
       .then((res) => {
         console.log(res.data.hits);
@@ -23,15 +28,50 @@ const App = () => {
     fetchData();
   }, []);
 
-  // console.log(news);
-
   return (
-    <div>
-      {news.map((item) => (
-        // <Card {...item} key={item.objectID} />
-        <BasicCard {...item} key={item.objectID} />
-      ))}
-    </div>
+    <>
+      <AppBar position="relative">
+        <Toolbar className={classes.toolBar}>
+          <div className={classes.imgWrapper}>
+            <img src={logo} className={classes.imgClass} />
+            <Typography variant="h4">Hacker News</Typography>
+          </div>
+          <form>
+            <input
+              type="text"
+              className={classes.inputField}
+              placeholder="  Search…"
+            />
+            <Button
+              type="submit"
+              variant="outlined"
+              className={classes.searchButton}
+              size="medium"
+              style={{ color: "#FFFFFF", borderColor: "#FFFFFF" }}
+              startIcon={<SearchIcon />}
+            >
+              Search
+            </Button>
+          </form>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="lg">
+            {news.map((item) => (
+              <div className={classes.card}>
+                <BasicCard
+                  {...item}
+                  key={item.objectID}
+                  className={classes.card}
+                />
+              </div>
+            ))}
+          </Container>
+        </div>
+      </main>
+    </>
   );
 };
 
---
+++ src/components/Styles.js
@@ -0,0 +1,49 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { NoEncryptionTwoTone } from "@mui/icons-material";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(4, 0, 8, 0),
+  },
+
+  toolBar: {
+    justifyContent: "space-between",
+  },
+
+  card: {
+    padding: "20px",
+  },
+
+  title_href: {
+    textDecoration: "none",
+    color: "rgba(0,0,0,0.87)",
+  },
+
+  imgWrapper: {
+    display: "flex",
+    alignItems: "center",
+    padding: "25px 0",
+  },
+
+  imgClass: {
+    height: "100px",
+  },
+
+  inputField: {
+    height: "30px",
+    width: "250px",
+    opacity: "0.8",
+    marginRight: "10px",
+    borderRadius: "3px",
+    border: "1px solid #ffffff",
+  },
+
+  searchButton: {
+    color: "#fff",
+    height: "35px",
+    opacity: "0.7",
+  },
+}));
+
+export default useStyles;
